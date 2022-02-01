98049ad7ec1c64fac8fa3969cf4316537f1f1b22
krishnadev v <krishnadevv12@gmail.comgit config --global user.name krishnadev7>
2022-02-01 16:21:55 +0530

    React Netflix sider commited

--- src/home/Home.jsx
+++ src/home/Home.jsx
@@ -1,13 +1,16 @@
-import { AcUnit } from '@material-ui/icons';
 import React from 'react';
 import Featured from '../components/featured/Featured';
 import Navbar from '../components/navbar/Navbar';
 import './home.scss';
+import List from '../components/list/List'
 
 const Home = () => {
   return <div className='home'>
        <Navbar />
-        <Featured type={"movie"}/>
+        <Featured />
+        <List/>
+        <List/>
+        <List/>
         </div>;
 };
 
